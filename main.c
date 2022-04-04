20a15b70ca2946f0d956056dc896d92db750f067
Adem Hatay <hatayadem5@gmail.com>
2022-04-04 10:51:05 +0300

    main

--- main.c
+++ main.c
@@ -16,26 +16,28 @@ int	main(void)
 	ft_printf("-------------------------------------------------\n");
 	
 	ft_printf("----------------     %%s    ---------------------\n");
-	char dizi[] = "henüz yazılmadı...:/\n";
+	char dizi[] = "Merhaba Dünya\n";
 	ft_printf( dizi);
 	ft_printf("-------------------------------------------------\n");
 
     ft_printf("----------------     %%d & i    ---------------------\n");
-	char dizi1[] = "henüz yazılmadı...:/\n";
-	ft_printf( dizi1);
+	int number1 = 127381;
+	int number2 = -1872381;
+	ft_printf("%d\n",number1);
+	ft_printf("%i\n",number2);
 	ft_printf("-------------------------------------------------\n");
 
     ft_printf("----------------     %%p    ---------------------\n");
 	char dizi2[] = "henüz yazılmadı...:/\n";
-	ft_printf( dizi2);
+	ft_printf("%p\n", dizi2);
 	ft_printf("-------------------------------------------------\n");
 
     ft_printf("----------------     %%X & x    ---------------------\n");
-	char dizi3[] = "henüz yazılmadı...:/\n";
-	ft_printf( dizi3);
+	int ca = 28;
+	ft_printf("%x\n", ca);
 	ft_printf("-------------------------------------------------\n");
 
-    // gcc main.c ft_print_str.c ft_printf.c ft_print_point.c  ft_print_unsigned.c ft_print_int.c 
+    // gcc main.c ft_print_str.c ft_printf.c ft_print_point.c  ft_print_unsigned.c ft_print_int.c ft_print_hex.c ft_putchar_fd.c
 
 	return (0);
 }
